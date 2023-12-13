8b6d090b991e152fa6f05ed30a89c93721dde050
lautarose <1714186@ucc.edu.ar>
2023-12-13 04:31:01 -0300

    updating url mapping

--- app/url-mapping.go
+++ app/url-mapping.go
@@ -11,5 +11,6 @@ func MapUrls() {
 	router.GET("/activity/:id", activityController.GetActivityById)
 	router.GET("/activities/user/:id", activityController.GetActivitiesByUserId)
 	router.POST("/activity", activityController.InsertActivity)
+	router.DELETE("/activity/:id", activityController.InsertActivity)
 
 }
